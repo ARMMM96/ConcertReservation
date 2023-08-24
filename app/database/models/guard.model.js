940496986fbe4de48c8f525f6851b3d9ee9c7438
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-08-24 08:22:36 +0300

    guard model updated
        concerts and rates are added to the model
        and the complaints changed to be optional not required

--- app/database/models/guard.model.js
+++ app/database/models/guard.model.js
@@ -10,12 +10,17 @@ const organizerSchema = mongoose.Schema({
     password: User.schema.paths.password,
     phoneNumber: User.schema.paths.phoneNumber,
     role: User.schema.paths.role,
-    concerts: [],
-    reate: [],
+    concerts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Concert',
+    }],
+    rates: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Rate',
+    }],
     complaints: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Complaint',
-        required: true
     }]
 
 
