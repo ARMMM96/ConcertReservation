be22c3dec9947086a2c6411811f6483be2007c40
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-08-24 06:40:34 +0300

    guard model implemented

---
+++ app/database/models/guard.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const User = require('./user');
+
+const organizerSchema = mongoose.Schema({
+    // Inherit properties from User model
+    firstName: User.schema.paths.firstName,
+    lastName: User.schema.paths.lastName,
+    email: User.schema.paths.email,
+    image: User.schema.paths.image,
+    password: User.schema.paths.password,
+    phoneNumber: User.schema.paths.phoneNumber,
+    role: User.schema.paths.role,
+    concerts: [],
+    reate: []
+
+
+});
+
+const Organizer = mongoose.model('Organizer', organizerSchema);
+
+module.exports = Organizer;
