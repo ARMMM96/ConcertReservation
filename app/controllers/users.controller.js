eac327ae1b2344f4e45eb86c685c3e3504c4ecb2
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-08-24 09:39:45 +0300

    User controller impelemnted
        This controller will be responsible
        for doing CRUD operations on the user model.

---
+++ app/controllers/users.controller.js
@@ -0,0 +1,182 @@
+const userModel = require('../database/models/user.model')
+const helper = require("../helpers/helpers")
+const multer = require("multer")
+const bcryptjs = require("bcryptjs")
+const upload = require('../middlewares/imageUpload.middleware')
+
+class User {
+    static register = async (req, res) => {
+        try {
+            const userData = new userModel(req.body)
+            await userData.save()
+
+            helper.responseHandler(res, 201, true, userData, "User registered successfully")
+        }
+        catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+
+    static login = async (req, res) => {
+        try {
+            const userData = await userModel.loginUser(req.body.email, req.body.password)
+            const token = await userData.generateToken()
+            helper.responseHandler(res, 200, true, { user: userData, token }, "user logedIn successfully")
+        }
+        catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+    static profile = (req, res) => {
+        helper.responseHandler(res, 200, true, { user: req.user }, "user profile fetched")
+    }
+    static getSingleUser = async (req, res) => {
+        const id = req.params.id;
+        try {
+            const userData = await userModel.findById(id).populate('role').populate('company');
+            if (!userData) {
+                helper.responseHandler(res, 404, false, null, "User Is not exist")
+
+            } else {
+
+                helper.responseHandler(res, 200, true, userData, "User found")
+            }
+        }
+        catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+    static getAllUsers = async (req, res) => {
+        try {
+            const usersData = await userModel.find().populate('role').populate('company');
+            if (!usersData || usersData.length == 0) {
+                helper.responseHandler(res, 404, false, null, "Users Is not exist")
+
+            } else {
+
+                helper.responseHandler(res, 200, true, usersData, "User found")
+            }
+        }
+        catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+    static updateUserData = async (req, res) => {
+        try {
+            const userData = await userModel.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                { new: true }
+            );
+            if (!userData) {
+                helper.responseHandler(res, 404, false, null, "User Is not exist")
+            } else {
+
+                helper.responseHandler(res, 200, true, userData, "User updated")
+            }
+        }
+        catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+
+    static deleteUser = async (req, res) => {
+        try {
+            const userData = await userModel.findByIdAndRemove(req.params.id)
+
+            if (!userData) {
+                helper.responseHandler(res, 404, false, null, "User Is not exist")
+            } else {
+
+                helper.responseHandler(res, 200, true, userData, "User deleted")
+            }
+
+        } catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+
+    static profilePicture = async (req, res) => {
+        try {
+            const uploadImage = upload.single('img')
+            uploadImage(req, res, async function (err) {
+                if (err instanceof multer.MulterError) {
+                    return res.send({ err: "invalid upload" })
+                } else if (err) {
+                    return res.send({ err: "invalid upload 1" })
+                }
+                req.user.image = req.file.path
+                await req.user.save()
+                helper.responseHandler(res, 200, true, req.user, "updated")
+            })
+        }
+        catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+
+
+    static async getCompanyUsers(req, res) {
+
+        const companyId = req.params.id;
+        try {
+            const userData = await userModel.find(
+                {
+                    "companyId": companyId
+                }
+            );
+            if (!userData) {
+                helper.responseHandler(res, 404, false, null, "Compan users User Is not exist")
+
+            } else {
+
+                helper.responseHandler(res, 200, true, userData, "User found")
+            }
+        }
+        catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+
+    static async changePassword(req, res) {
+        try {
+
+            const { currentPassword, newPassword } = req.body; // Get the user ID, current password, and new password from the request body
+
+            const id = req.params.id;
+            const user = await userModel.findById(id); // Find the user based on their ID
+
+            if (!user) {
+                helper.responseHandler(res, 404, false, user, "User not found")
+
+            } else {
+                const isMatch = await bcryptjs.compare(currentPassword, user.password);
+                if (!isMatch) {
+
+                    helper.responseHandler(res, 400, false, null, "Current password is incorrect")
+
+                } else {
+
+                    const userData = await userModel.findByIdAndUpdate(
+                        req.params.id,
+                        { "password": newPassword },
+                        { new: true }
+                    );
+
+                }
+                helper.responseHandler(res, 200, true, user, "Password changed successfully")
+            }
+
+        } catch (e) {
+            console.error(e.message);
+            helper.responseHandler(res, 500, false, e, e.message)
+
+        }
+    }
+
+    static profile = (req, res) => {
+        helper.responseHandler(res, 200, true, { user: req.user }, "user profile fetched")
+    }
+
+}
+module.exports = User
