d7b8fe2cffe0035c59fdfde5cd60c473b338b123
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-08-24 07:33:50 +0300

    guard model updated
        model updated to make reference
        between the complaints and the guard model

--- app/database/models/guard.model.js
+++ app/database/models/guard.model.js
@@ -11,7 +11,11 @@ const organizerSchema = mongoose.Schema({
     phoneNumber: User.schema.paths.phoneNumber,
     role: User.schema.paths.role,
     concerts: [],
-    reate: []
+    reate: [],
+    complaints:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Role',
+    }]
 
 
 });
