f4d5317e4b2bb273ce94b5fc7947b2b393ba5282
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-08-24 07:30:06 +0300

    complaint model implemented
    this model created so the guests can create complaint
    if they experienced an unpleasant treatment
    from any of the guards or other employees

---
+++ app/database/models/complaint.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const complaintSchema = new mongoose.Schema({
+    complaintSubject: {
+        type: String,
+        required: true,
+    },
+    details: {
+        type: String,
+    },
+    guardId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+    },
+});
+
+const Complaint = mongoose.model("Complaint", complaintSchema);
+
+module.exports = Complaint;
