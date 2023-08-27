83c07757e74b4175d9e3713c2346b41ddebb19f9
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-08-27 04:48:02 +0300

    Roles controller implemented
        This controller will be responsible
        for performing CRUD operations on the Role model.

---
+++ app/controllers/roles.controller.js
@@ -0,0 +1,74 @@
+const rolesModel = require('../database/models/role.model')
+const helper = require("../helpers/helpers")
+class Role {
+    static createRole = async (req, res) => {
+        try {
+            const rolesData = new rolesModel(req.body)
+            await rolesData.save()
+            helper.responseHandler(res, 201, true, rolesData, "Role added successfully")
+        }
+        catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+    static getRoles = async (req, res) => {
+        try {
+            const rolesData = await rolesModel.find();
+            if (!rolesData) {
+                helper.responseHandler(res, 404, false, null, "Role Is not exist")
+            } else {
+
+                helper.responseHandler(res, 200, true, rolesData, "Roles found")
+            }
+        }
+        catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+    static getRole = async (req, res) => {
+        try {
+            const rolesData = await rolesModel.findById(req.params.id);
+            if (!rolesData) {
+                helper.responseHandler(res, 404, false, null, "Role Is not exist")
+            } else {
+
+                helper.responseHandler(res, 200, true, rolesData, "Roles found")
+            }
+        }
+        catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+    static updateRole = async (req, res) => {
+        try {
+            const rolesData = await rolesModel.findOneAndUpdate(
+                { _id: req.params.id, },
+                { ...req.body },
+                { new: true }
+            );
+            if (!rolesData) {
+                helper.responseHandler(res, 404, false, null, "Role Is not exist")
+            } else {
+
+                helper.responseHandler(res, 200, true, rolesData, "Role updated")
+            }
+        }
+        catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+
+    static deleteRole = async (req, res) => {
+        try {
+            const rolesData = await rolesModel.findByIdAndRemove(req.params.id)
+            if (!rolesData) {
+                helper.responseHandler(res, 404, false, null, "Role Is not exist")
+            } else {
+                helper.responseHandler(res, 200, true, rolesData, "Role deleted")
+            }
+        } catch (e) {
+            helper.responseHandler(res, 500, false, e, e.message)
+        }
+    }
+}
+module.exports = Role
